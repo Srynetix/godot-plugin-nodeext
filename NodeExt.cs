2253cf35624f19785961e7fc3e195ad40f8ee471
Denis BOURGE <bourge.denis@gmail.com>
2021-09-30 21:30:43 +0200

    feat: BindNodes

--- NodeExt.cs
+++ NodeExt.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using Godot;
 
 namespace SxGD
 {
@@ -43,5 +44,27 @@ namespace SxGD
     public class OnReady : OnReadyBase
     {
         public OnReady(string path = "") : base(path, false) { }
+    }
+
+    public static class NodeExt
+    {
+        /// Automatically bind nodes with a OnReadyBase attribute.
+        /// <c>OnReady</c> will get a node corresponding to the type name, unless a path is explicitly given.
+        public static void BindNodes(Node node)
+        {
+            var fields = node.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var field in fields)
+            {
+                var customAttr = (OnReadyBase)field.GetCustomAttribute(typeof(OnReadyBase));
+                if (customAttr != null)
+                {
+                    var bindPath = customAttr.GetNodePath(field);
+
+                    // Bind
+                    var nodeInstance = node.GetNode(bindPath);
+                    field.SetValue(node, nodeInstance);
+                }
+            }
+        }
     }
 }
