cdda0749947d5e2859975bd84230cbbbc2d43f0c
Denis BOURGE <bourge.denis@gmail.com>
2021-10-02 23:46:59 +0200

    feat: SceneTransitioner

---
+++ SceneTransitioner.cs
@@ -0,0 +1,49 @@
+using Godot;
+
+namespace SxGD
+{
+    public class SceneTransitioner : CanvasLayer
+    {
+        [OnReady("Overlay")]
+        private ColorRect _Overlay;
+        [OnReady]
+        private AnimationPlayer _AnimationPlayer;
+
+        private static SceneTransitioner _GlobalInstance;
+
+        public SceneTransitioner()
+        {
+            if (_GlobalInstance == null)
+            {
+                _GlobalInstance = this;
+            }
+        }
+
+        public static SceneTransitioner GetInstance()
+        {
+            return _GlobalInstance;
+        }
+
+        public override void _Ready()
+        {
+            NodeExt.BindNodes(this);
+        }
+
+        public async void FadeToScene(string path)
+        {
+            _AnimationPlayer.Play("fade_out");
+            var scene = GD.Load<PackedScene>(path);
+            await ToSignal(_AnimationPlayer, "animation_finished");
+            GetTree().ChangeSceneTo(scene);
+            _AnimationPlayer.Play("fade_in");
+        }
+
+        public async void FadeToScene(PackedScene scene)
+        {
+            _AnimationPlayer.Play("fade_out");
+            await ToSignal(_AnimationPlayer, "animation_finished");
+            GetTree().ChangeSceneTo(scene);
+            _AnimationPlayer.Play("fade_in");
+        }
+    }
+}
